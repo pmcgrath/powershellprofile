6172c5af24f747d2985dea2804ea7000dd1a067b
unknown <pmcgrath@.ccs.local>
2010-07-31 12:20:07 +0100

    .

--- General.ps1
+++ General.ps1
@@ -17,3 +17,13 @@ function np
 {
 	if (test-path 'C:\Program Files\Notepad++') { &'C:\Program Files\Notepad++\Notepad++.exe' $args; }
 }
+
+
+# Ensure the following are in the path
+
+# Ensure Git is on current path
+if (! ($env:Path -contains 'C:\Program Files\Git\bin')) { $env:Path += ';C:\Program Files\Git\bin'; }
+
+# Ensure Ruby.exe is on current path - Currently v1.9.1 but may need to support multiple versions soon
+if (! ($env:Path -contains 'C:\ruby\Ruby191\bin')) { $env:Path += ';C:\ruby\Ruby191\bin'; }
+
