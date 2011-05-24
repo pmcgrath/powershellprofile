e5e8bf4cb0ecfa30110fade4c81745c203eeac07
pmcgrath <pmcgrath@gmail.com>
2011-05-25 00:36:00 +0100

    Merged some of Daves functions

--- General.ps1
+++ General.ps1
@@ -1,4 +1,4 @@
-# Set browser proxy usage - does not alter already running instances of IE
+# Functions
 function Set-BrowserProxyUsage
 (
 	[switch] $useProxy
@@ -15,15 +15,6 @@ function Set-BrowserProxyUsage
 	pop-location;
 }
 
-
-# Notepad++
-function NP
-{
-	if (test-path 'c:\program files\notepad++') { &'c:\program files\notepad++\notepad++.exe' $args; }
-}
-
-
-# Extend environment path
 function Extend-EnvironmentPath
 (
 	[string[]] $additions
@@ -32,8 +23,6 @@ function Extend-EnvironmentPath
 	$additions | % { if (! ($env:Path.Contains($_))) { $env:Path += ";$_"; }}
 }
 
-
-# Set ruby environment
 function Set-RubyEnvironment
 (
 	[string] $version	= '192_p136'
@@ -49,8 +38,15 @@ function Set-RubyEnvironment
 	else 					{ Extend-EnvironmentPath @($newRubyEnvironmentPath); }
 }
 
+function Test-IsCurrentUserAnAdministrator
+{
+	(new-object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole("Administrators");
+}
 
-# Ensure the following are in the path
+# Change foreground colour if an admin
+if (Test-IsCurrentUserAnAdministrator -and ($host.Name -eq 'ConsoleHost')) { $host.UI.RawUI.ForegroundColor = 'Red'; }
+
+# Ensure the following are in the path - if they don't exist will ignore anyway
 $pathAdditions = @();
 $pathAdditions += 'c:\program files\git\bin';									# Git
 $pathAdditions += 'c:\windows\microsoft.net\framework\v4.0.30319';				# .Net 4.0
@@ -58,3 +54,7 @@ Extend-EnvironmentPath $pathAdditions;
 
 # Set up default ruby
 Set-RubyEnvironment;
+
+# Common aliases - assume they exist rather than clutering with tests
+set-alias np "c:\program files\notepad++\notepad++.exe";
+set-alias ie "c:\program files\internet explorer\iexplore.exe";
