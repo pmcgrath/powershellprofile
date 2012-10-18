74d45f23295b5acfb6cf732948aa9d83003075aa
pmcgrath <pmcgrath@gmail.com>
2012-10-18 23:23:26 +0100

    Added start ssh agent function - based on https://github.com/dahlbyk/posh-git

--- common-functions.ps1
+++ common-functions.ps1
@@ -10,6 +10,25 @@ function Extend-EnvironmentPath
 	$additions | % { if (! ($env:Path.Contains($_))) { $env:Path += ";$_"; }}	# Can see existing with $env:Path.Split(';')
 }
 
+function Start-SshAgent
+{
+	# This is based on https://github.com/dahlbyk/posh-git
+	# Ensure ssh agent is already running - will use one per session currently
+	if (-not $env:SSH_AGENT_PID)
+	{
+		ssh-agent | ? { $_ -match '(?<key>[^=]+)=(?<value>[^;]+);' | % { $key = $matches['key']; $value =$matches['value']; set-item env:$key $value; } }
+	}
+	
+	# Add entry if key exists and no entry already - assuming the name and location of key is default
+	if (test-path ~/.ssh/id_rsa)
+	{
+		if (-not (ssh-add -l | ? { $_ -match ".*/users/$env:UserName/\.ssh/id_rsa" }))
+		{
+			ssh-add ~/.ssh/id_rsa;
+		}
+	}
+}
+
 function Set-RubyEnvironment
 (
 	[string] $version	= '1.9.3-194'
